e883f75b783eb3a856e9fa899f7d01e57b8c6c54
Alexander Tolkachev <atolkachev@AT-MacBook-Pro-5.local>
2017-11-19 17:39:49 +0300

    Implement own get_cidr

--- library/get_ip_facts.py
+++ library/get_ip_facts.py
@@ -3,7 +3,17 @@
 
 import subprocess
 from ansible.module_utils.basic import *
-from netaddr import IPNetwork
+
+def get_cidr(ipaddr):
+    addr = ipaddr[0:ipaddr.find("/")].split(".")
+    mask = int(ipaddr[ipaddr.find("/") + 1:])
+
+    wildcard = 32 - mask
+    bin_addr = (int(addr[0]) << 24) + (int(addr[1]) << 16) + (int(addr[2]) << 8) + int(addr[3])
+    cidr_bin = bin_addr - (bin_addr % (2 ** wildcard))
+    cidr = '.'.join([str(cidr_bin >> (i << 3) & 0xFF) for i in range(4)[::-1]])
+
+    return "{0}/{1}".format(cidr, mask)
 
 def get_priv_interface_info_for_ip(ip, network_config):
     interface = ""
@@ -13,7 +23,7 @@ def get_priv_interface_info_for_ip(ip, network_config):
         if 'inet ' in str:
             if ip in str:
                 interface = str[3:str.find(" ", 3)]
-                network = IPNetwork(str[str.find("net", 3) + 4:str.find("/", 3) + 3]).cidr
+                network = get_cidr(str[str.find("net", 3) + 4:str.find("/", 3) + 3])
 
     return interface, ip_addr, network
 
@@ -24,9 +34,8 @@ def get_pub_interface_info_for_ip(ip, network_config):
     for str in network_config.split("\n"):
         if 'inet ' in str:
             if ip not in str and '127.0.0.1' not in str:
-                print str
                 interface = str[3:str.find(" ", 3)]
-                network = IPNetwork(str[str.find("net", 3) + 4:str.find("/", 3) + 3]).cidr
+                network = get_cidr(str[str.find("net", 3) + 4:str.find("/", 3) + 3])
                 ip_addr = str[str.find("net", 3) + 4:str.find("/", 3)]
 
     return interface, ip_addr, network
